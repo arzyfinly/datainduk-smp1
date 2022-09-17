563c9a86148f279dc07e4879c0e9c65d3efa0e15
dewirohani <dewirohani273@gmail.com>
2022-09-17 13:05:07 +0700

    show

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,7 @@
     <!-- endinject -->
     <!-- Layout styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/demo_1/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" />
   </head>
